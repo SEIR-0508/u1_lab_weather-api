bcdc169b056c1aa8eee1c2464d74656df6f79436
chrischen <chrischen96@outlook.com>
2023-05-22 13:14:47 -0700

    finished

--- script.js
+++ script.js
@@ -12,23 +12,13 @@ $('#submitButton').on('click', async () => {
         }
     })
     let weather = weatherRes.data
-    // let location = [weather.location.name, weather.location.region, weather.location.country]
-    // let current = [weather.current.temp_f, weather.current.humiditu, weather.current.cloud, weather.current.feelslike_f, weather.current.uv, weather.current.air_quality.pm2_5]
     let location = ['name', 'region', 'country', 'localtime']
     let locationDis = ['Name', 'Region', 'Country', 'Local Time']
-    let current = ['temp_f', 'precip_in', 'humidity', 'wind_mph', 'pressure_in', 'feelslike_f', 'uv']
-    let currentDis = ['Temperature', 'Precipitation', 'Humidity', 'Wind', 'Air Pressure', 'Feeks Like', 'UV Index']
-    let currentUnit = ['&#8457;', '%', '%', 'mph', 'inHg', '&#8457;', '']
-
+    let current = ['temp_f', 'precip_in', 'humidity', 'wind_mph', 'vis_miles', 'pressure_in', 'feelslike_f', 'uv']
+    let currentDis = ['Temperature', 'Precipitation', 'Humidity', 'Wind', 'Visibility', 'Air Pressure', 'Feeks Like', 'UV Index']
+    let currentUnit = ['˚F', '%', '%', ' mph', ' mi', ' inHg', '˚F', '']
 
     console.log(weather)
-
-    // for (let prop of location) {
-    //     console.log(prop)
-    //     $('<h2>', {id: prop, html: prop + ': ' + weather.location[prop]}).appendTo($('#location'))
-    // }
-
-
 
     for (let prop of location) {
         console.log(prop, weather.location[prop])
@@ -45,17 +35,7 @@ $('#submitButton').on('click', async () => {
         console.log(prop)
         let index = current.indexOf(prop)
         $('<p>', {id: prop, html: currentDis[index] + ': ' + weather.current[prop] + currentUnit[index]}).appendTo($('#current'))
-    //     if (prop == 'air_quality') {
-    //         $('<div>', {id: prop}).html(prop).appendTo($('#current'))
-    //         $('<ul>').appendTo($('#' + prop))
-    //         for (let air in weather.current.air_quality) {
-    //             $('<li>', {id : air}).html(air + ': ' + weather.current.air_quality[air]).appendTo($('#' + prop + ' ul'))
-    //         }
-    //     } else if (prop == 'condition') {
-    //         $('<img>', {id: 'icon', src: weather.current.condition.icon}).appendTo($('#current'))
-    //     } else {
-    //         //console.log(prop, weather.current[prop])
-    //         $('<p>', {id: prop}).html(prop + ': ' + weather.current[prop]).appendTo($('#current'))
-    //     }
     }
+
+    $('<p>', {id: 'pm2_5', html: 'PM 2.5: ' + Math.floor(weather.current.air_quality.pm2_5) + ' μg/m3'}).appendTo($('#current'))
 })
