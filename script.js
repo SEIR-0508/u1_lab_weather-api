800663e564fd8b43c24d15bb549287397d2e6542
theallSparky <harrisonsparks.1999@gmail.com>
2023-06-12 17:16:16 -0500

    Added the state as visible to user

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ button.addEventListener('click', async () => {
     let textInput = document.querySelector("#textInput").value
     let weatherResponse = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${textInput}&aqi=no`)
     let locationData = weatherResponse.data.current
+    let region = weatherResponse.data.location.region
 
     console.log(textInput)
     console.log(weatherResponse)
@@ -24,6 +25,7 @@ button.addEventListener('click', async () => {
 
 
     document.getElementById("cityName").innerHTML = `Thank you for taking such an interest in the forecast for ${textInput}`
+    document.getElementById("region").innerHTML = `The state is `
     document.getElementById("tempF").innerHTML = `The temperature in ${textInput} in degrees farenheit is: ${tempF}`
     document.getElementById("tempC").innerHTML = `The temperature in ${textInput} in degrees celsius is: ${tempC}`
     document.getElementById("cloud").innerHTML = `The level of cloudinss on a scale of 1 - 100 in ${textInput} is: ${cloud}`
@@ -31,5 +33,7 @@ button.addEventListener('click', async () => {
     document.getElementById("windDir").innerHTML = `The wind direction in ${textInput} is: ${windDir}`
     document.getElementById("windSpeedMPH").innerHTML = `The wind speed in Miles per Hour in ${textInput} is: ${windSpeedMPH}`
     document.getElementById("windSpeedKPH").innerHTML = `The wind speed in Kilometers per Hour in ${textInput} is: ${windSpeedKPH}`
+    document.getElementById("region").innerHTML = `The state that ${textInput} is in is : ${region}`
+
 
 })
